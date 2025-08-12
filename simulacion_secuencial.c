f6ff0faba982bb3b826a55c0c6bcaa3d0f95f932
DiegoLinares11 <lin221256@uvg.edu.gt>
2025-08-12 13:34:27 -0600

    feat: basic structure that both simulations will have

--- simulacion_secuencial.c
+++ simulacion_secuencial.c
@@ -1,1 +1,28 @@
-// Falta implementacion
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Estados de luz: 0=ROJO, 1=VERDE, 2=AMARILLO
+typedef struct {
+    int id;
+    int estado;         // 0,1,2
+    int ticks_en_estado;
+    int dur_rojo, dur_verde, dur_amarillo;
+} TrafficLight;
+
+typedef struct {
+    int id;
+    int posicion;       // celda discreta 0..RUTA_MAX
+    int velocidad;      // celdas/iteración cuando hay verde
+    int destino;        // posición objetivo (simple)
+    int luz_asociada;   // índice de semáforo que controla su paso
+} Vehicle;
+
+typedef struct {
+    TrafficLight *lights;
+    int num_lights;
+    Vehicle *vehicles;
+    int num_vehicles;
+    int ruta_max;       // largo de la ruta discreta
+} Interseccion;
+
